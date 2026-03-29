9834778f12611b6f253c34265c46fb4eb90284d3
opnct <108859005+opnct@users.noreply.github.com>
2026-03-29 19:55:05 +0000

    u

--- vyaparsetu-desktop/electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose a secure API to the React frontend
-contextBridge.exposeInMainWorld('electronAPI', {
-  // Acts identically to Tauri's invoke command
-  invoke: (channel, data) => ipcRenderer.invoke(channel, data)
-});
