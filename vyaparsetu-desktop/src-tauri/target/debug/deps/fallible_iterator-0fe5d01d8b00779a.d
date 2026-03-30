afd9c9eac6d0da8df12369f67bdd0d3c79c3116c
opnct <108859005+opnct@users.noreply.github.com>
2026-03-30 06:38:55 +0000

    u

---
+++ vyaparsetu-desktop/src-tauri/target/debug/deps/fallible_iterator-0fe5d01d8b00779a.d
@@ -0,0 +1,7 @@
+/workspaces/vs/vyaparsetu-desktop/src-tauri/target/debug/deps/fallible_iterator-0fe5d01d8b00779a.d: /usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fallible-iterator-0.2.0/src/lib.rs
+
+/workspaces/vs/vyaparsetu-desktop/src-tauri/target/debug/deps/libfallible_iterator-0fe5d01d8b00779a.rlib: /usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fallible-iterator-0.2.0/src/lib.rs
+
+/workspaces/vs/vyaparsetu-desktop/src-tauri/target/debug/deps/libfallible_iterator-0fe5d01d8b00779a.rmeta: /usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fallible-iterator-0.2.0/src/lib.rs
+
+/usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/fallible-iterator-0.2.0/src/lib.rs:
---
+++ vyaparsetu-desktop/src-tauri/target/debug/deps/vyaparsetu-c97b26b199dbba87.d
@@ -0,0 +1,8 @@
+/workspaces/vs/vyaparsetu-desktop/src-tauri/target/debug/deps/vyaparsetu-c97b26b199dbba87.d: src/main.rs src/db.rs src/commands.rs src/schema.sql
+
+/workspaces/vs/vyaparsetu-desktop/src-tauri/target/debug/deps/vyaparsetu-c97b26b199dbba87: src/main.rs src/db.rs src/commands.rs src/schema.sql
+
+src/main.rs:
+src/db.rs:
+src/commands.rs:
+src/schema.sql:
